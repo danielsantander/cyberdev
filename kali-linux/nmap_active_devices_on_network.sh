3ac16bb50918b4b375c050fd7300ff4e0904cab8
danielsantander <santander9108@gmail.com>
2021-09-05 04:13:40 -0500

    fix redundancy in variable name

--- kali-linux/nmap_active_devices_on_network.sh
+++ kali-linux/nmap_active_devices_on_network.sh
@@ -2,9 +2,9 @@
 
 if [ -z "$1" ]
   then
-    cidr_range=$(ip --brief address show | awk '$3 ~ /^[0-9]{1,3}(\.[0-9]{1,3}){3}\/[0-9]{2}$/ {print $3}')
+    cidr=$(ip --brief address show | awk '$3 ~ /^[0-9]{1,3}(\.[0-9]{1,3}){3}\/[0-9]{2}$/ {print $3}')
 else
-    cidr_range=$1;
+    cidr=$1;
 fi
 
 # Use expansion operator -> ${parameter:+[word]}
@@ -12,14 +12,14 @@ fi
 #    If parameter is unset or null, null shall be substituted;
 #    otherwise, the expansion of word (or an empty string if word is omitted)
 #    shall be substituted.
-if [ -z ${cidr_range+x} ]
+if [ -z ${cidr+x} ]
 then
-    echo "ip address range ($cidr_range) is unset, exiting.";
+    echo "ip address range ($cidr) is unset, exiting.";
     exit 1;
 else
-    echo "performing nmap on $cidr_range ..."
+    echo "performing nmap on $cidr ..."
 
-    network=$(nmap -sP $cidr_range | awk '$NF ~ /[0-9]+(\.[0-9]+){3}/ {print $NF}')
+    network=$(nmap -sP $cidr | awk '$NF ~ /[0-9]+(\.[0-9]+){3}/ {print $NF}')
     echo "IP addresses found: "
     echo "$network"
 fi;
