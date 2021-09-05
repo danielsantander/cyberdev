7431bcab9148e192aaba1c08267a3f67f878bd3c
danielsantander <santander9108@gmail.com>
2021-09-05 04:05:46 -0500

    change variable ip_range to cidr_range to be more specific

--- kali-linux/nmap_active_devices_on_network.sh
+++ kali-linux/nmap_active_devices_on_network.sh
@@ -2,9 +2,9 @@
 
 if [ -z "$1" ]
   then
-    ip_address_range=$(ip --brief address show | awk '$3 ~ /^[0-9]{1,3}(\.[0-9]{1,3}){3}\/[0-9]{2}$/ {print $3}')
+    cidr_range=$(ip --brief address show | awk '$3 ~ /^[0-9]{1,3}(\.[0-9]{1,3}){3}\/[0-9]{2}$/ {print $3}')
 else
-    ip_address_range=$1;
+    cidr_range=$1;
 fi
 
 # Use expansion operator -> ${parameter:+[word]}
@@ -12,14 +12,14 @@ fi
 #    If parameter is unset or null, null shall be substituted;
 #    otherwise, the expansion of word (or an empty string if word is omitted)
 #    shall be substituted.
-if [ -z ${ip_address_range+x} ]
+if [ -z ${cidr_range+x} ]
 then
-    echo "ip address range ($ip_address_range) is unset, exiting.";
+    echo "ip address range ($cidr_range) is unset, exiting.";
     exit 1;
 else
-    echo "performing nmap on $ip_address_range ..."
+    echo "performing nmap on $cidr_range ..."
 
-    network=$(nmap -sP $ip_address_range | awk '$NF ~ /[0-9]+(\.[0-9]+){3}/ {print $NF}')
+    network=$(nmap -sP $cidr_range | awk '$NF ~ /[0-9]+(\.[0-9]+){3}/ {print $NF}')
     echo "IP addresses found: "
     echo "$network"
 fi;
