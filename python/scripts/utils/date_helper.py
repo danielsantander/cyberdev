12082165db120fb140837515a8667d8f066627ea
danielsantander <santander9108@gmail.com>
2022-09-30 19:42:27 -0500

    set default value for setting timestamp to date string

--- python/scripts/utils/date_helper.py
+++ python/scripts/utils/date_helper.py
@@ -6,15 +6,15 @@ from os import times
 import pytz
 from pytz.tzinfo import DstTzInfo
 
-def timestamp_to_date_string(timestamp:int=datetime.datetime.utcnow().timestamp()) -> str:
-    """ Returns datetime string representation of given timestamp with format 'YYYYMMDDHHMMSS'. 
-    
+def timestamp_to_date_string(timestamp:int=datetime.datetime.utcnow().timestamp(), str_format:str="%Y%m%d%H%M%S") -> str:
+    """ Returns datetime string representation of given timestamp with format 'YYYYMMDDHHMMSS'.
+
     Keyword arguments:
     timestamp -- integer value representing the timestamp to convert (default datetime utcnow timestamp)
     """
     ts = int(str(timestamp).split(".")[0])
     date = datetime.datetime.fromtimestamp(ts)
-    return date.strftime("%Y%m%d%H%M%S")
+    return date.strftime(str_format)
 
 def current_iso_time(tz:DstTzInfo=pytz.timezone('UTC')) -> str:
     """ Return current iso time as str in format YYYY-MM-DDTHH:mm:ssZ """
