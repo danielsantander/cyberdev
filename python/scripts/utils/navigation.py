a0ff8f9b9e12494cbf3daed5e62499f2539c5e96
danielsantander <santander9108@gmail.com>
2023-04-28 19:05:43 -0700

    add method to move files from a given source directory to a given destination directory

--- python/scripts/utils/navigation.py
+++ python/scripts/utils/navigation.py
@@ -1,10 +1,11 @@
+#!/usr/bin/env python
 #!/usr/bin/python3
 
 """ Unique methods to handle navigating through the operating system. """
-import errno
-import os
+import logging
+import shutil
 from pathlib import Path
-from typing import Union, List
+from typing import List, Optional, Union
 from utils.custom_exceptions import InvalidDirectory
 
 def get_filenames(directory_path: Union[str,Path], return_as_path:bool=False) -> List[Union[str, Path]]:
@@ -47,3 +48,41 @@ def clean_directory(directory_path: Union[str, Path], remove_directory: bool=Fal
     if remove_directory: p.rmdir()
     return p
 
+def move_file(file_src:Path, dst_dir:Path, rename:str=None, lgr:logging.Logger=None)->Optional[Path]:
+    """
+    Move a file from given source path (file_src) to the given directory path (dst_dir).
+    Returns moved filepath if file successfully moved, else returns None.
+
+    Keyword arguments:
+    file_src -- The source file path to move.
+    dst_dir -- The destination of the directory to move file into.
+    rename -- A new name to give the file to move.
+    lgr (optional) -- optional logger
+
+    """
+    assert file_src.exists() and file_src.is_file()
+
+    if not dst_dir.exists() or not dst_dir.is_dir():
+        dst_dir = make_directory(dst_dir)
+    assert dst_dir.exists() and dst_dir.is_dir()
+
+    # TODO: ensure rename does not have an extension in it
+    filename = str(file_src.name) if rename is None else str(rename + file_src.suffix)
+    target_file_path: Path = (dst_dir / filename)
+
+    if target_file_path.exists():
+        if lgr: lgr.debug(f'File already exists at destination path.')
+        return None
+
+    # TODO: add options to copy instead of only move
+
+    # Use `shutil` if moving file_src to dst_dir involves more than one system.
+    # target_file_path = media.rename(target_file_path)
+    target_file_path = Path(shutil.move(file_src.absolute(), target_file_path.absolute()))
+
+    try:
+        assert target_file_path.exists() and target_file_path.is_file()
+    except AssertionError as err:
+        if lgr: lgr.error(f'failed to move file {file_src.absolute()}')
+        return None
+    return target_file_path
