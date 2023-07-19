5f6bdb4fda0afc6fc86cb7a869eedaf403cd6ce9
Daniel Santander <dan.santander@digitaldefense.com>
2023-07-19 11:28:05 -0500

    cleanup script

--- kali-linux/scripts/gitgone.sh
+++ kali-linux/scripts/gitgone.sh
@@ -1,16 +1,22 @@
 #! /bin/bash
 
 pwd && echo "";
-# git repo
-cd /Users/danielsantander/code/repos/frontline-dev
+# get git repo
+echo -n 'Enter git repository path: '
+read dirpath
+cd "${dirpath}"
+
 
 # delete gone branches
 pwd && echo -e "\ndeleting the gone branches...\n";
+
+
 # git for-each-ref --format '%(refname:short) %(upstream:track)' | awk '$2 == "[gone]" {print $1}' | xargs -r git branch -D
 # xargs on Mac OS X doesn't support the --replace (-r) option, use -I instead
-# git for-each-ref --format '%(refname:short) %(upstream:track)' | awk '$2 == "[gone]" {print $1}' | xargs -I git branch -D
-git for-each-ref --format '%(refname:short) %(upstream:track)' | awk '$2 == "[gone]" {print $1}' | xargs -I {} echo 'git branch -D {}' # ECHO OUT FOR TESTING
-git for-each-ref --format '%(refname:short) %(upstream:track)' | awk '$2 == "[gone]" {print $1}' | xargs -I {} git branch -D {}
+
+
+# git for-each-ref --format '%(refname:short) %(upstream:track)' | awk '$2 == "[gone]" {print $1}' | xargs -I {} echo 'git branch -D {}' # ECHO OUT FOR TESTING
+git fetch -p && git for-each-ref --format '%(refname:short) %(upstream:track)' | awk '$2 == "[gone]" {print $1}' | xargs -I {} git branch -D {}
 
 
 echo -e "\n\n...finished."
