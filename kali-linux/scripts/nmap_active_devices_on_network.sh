dc39ea2e9171b7bb0ecbb9d507a4cdd8ff7be652
danielsantander <santander9108@gmail.com>
2023-08-05 23:03:29 -0500

    update script to handle case when results are found

--- kali-linux/scripts/nmap_active_devices_on_network.sh
+++ kali-linux/scripts/nmap_active_devices_on_network.sh
@@ -6,20 +6,28 @@ if [ -z "$1" ]
 else
     cidr=$1;
 fi
+echo""
 
 # Use expansion operator -> ${parameter:+[word]}
-#    Uses Alternative Value. 
+#    Uses Alternative Value.
 #    If parameter is unset or null, null shall be substituted;
 #    otherwise, the expansion of word (or an empty string if word is omitted)
 #    shall be substituted.
-if [ -z ${cidr+x} ]
-then
+if [ -z ${cidr+x} ]; then
     echo "No IP address range provided, exiting.";
     exit 1;
 else
-    echo "performing nmap on $cidr ..."
+    echo "performing nmap (sP scan) on $cidr ..."
 
     network=$(nmap -sP $cidr | awk '$NF ~ /[0-9]+(\.[0-9]+){3}/ {print $NF}')
-    echo "IP addresses found: "
-    echo "$network"
+
+    if [[ $? -gt 0 ]]; then
+        echo -e "\n\nIP RETRIEVAL FAILED\n"
+    elif [[ $? -eq 0 || -z $network ]]; then
+        echo -e "\nNO IPs FOUND\n";
+    else
+        echo -e "\n--------------------\nIP ADDRESSES FOUND:\n--------------------";
+        echo "$network"
+    fi
+    echo ""
 fi;
