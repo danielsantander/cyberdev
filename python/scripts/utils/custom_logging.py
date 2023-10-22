6db7090f87bbfd9219091439eb7c694888dbf8de
danielsantander <santander9108@gmail.com>
2023-10-22 17:39:01 -0500

    update custom logging to allow custom formatting

--- python/scripts/utils/custom_logging.py
+++ python/scripts/utils/custom_logging.py
@@ -14,22 +14,24 @@ CUR_DIR = os.path.abspath(os.path.dirname(__file__)) # utils/
 # LOG_DIR = os.path.join(CUR_DIR, "logs", "") # utils/logs/
 LOG_DIR = Path(CUR_DIR) / 'logs'    # utils/logs
 LOG_LEVEL: int = logging.DEBUG if DEBUG_MODE else logging.INFO
-LOG_FORMAT = logging.Formatter('%(asctime)s [%(levelname)s] %(name)s: %(message)s')
+LOG_FORMAT_STR = "%(asctime)s [%(levelname)s] %(name)s: %(message)s"
+LOG_FORMAT = logging.Formatter(LOG_FORMAT_STR)
 
 
-def create_console_logger(name:Optional[str]=None, level:int=LOG_LEVEL, format=LOG_FORMAT) -> logging.Logger:
+def create_console_logger(name:Optional[str]=None, level:int=LOG_LEVEL, format:str=None) -> logging.Logger:
     """
     Return a custom logger with stream (console) handler.
     """
+    log_format = logging.Formatter(format) if format is not None else LOG_FORMAT
     logger = logging.getLogger(name)
     console_handler = logging.StreamHandler()
     console_handler.setLevel(level)
-    console_handler.setFormatter(format)
+    console_handler.setFormatter(log_format)
     logger.addHandler(console_handler)
     logger.setLevel(level)
     return logger
 
-def create_logger(name:str, level:int=LOG_LEVEL, log_dir:Union[str,Path]=LOG_DIR, max_byte_size:int=10*1024*1024, backup_count:int=5) -> logging.Logger:
+def create_logger(name:str, level:int=LOG_LEVEL, log_dir:Union[str,Path]=LOG_DIR, max_byte_size:int=10*1024*1024, backup_count:int=5, format:str=None) -> logging.Logger:
     """
     Return logger with Stream and RotatingFile handlers.
     Creates log directory if one does not exist, and initializes a master_logger file.
@@ -39,23 +41,25 @@ def create_logger(name:str, level:int=LOG_LEVEL, log_dir:Union[str,Path]=LOG_DIR
     level -- logger level value defaults to logging.DEBUG (default 10)
     max_byte_size  -- max number of bytes for size of log file, defaults to 10 mb (default 10*1024*1024)
     backup_count -- number of backup files to keep in rotation (default 5)
+    format -- logger format
     """
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
     log_dir = log_dir if isinstance(log_dir, Path) else Path(log_dir)
     log_dir = navigation.make_directory(log_dir)
+    log_format = logging.Formatter(format) if format is not None else LOG_FORMAT
 
     # Master logger (holds all logs)
     master_location = os.path.join(log_dir, "master_logger.log")
     #max_byte_size_50 = 50*1024*1024 # ~52mb
     master_file_handler = logging.handlers.RotatingFileHandler(master_location, maxBytes=max_byte_size, backupCount=backup_count)
     master_file_handler.setLevel(logging.DEBUG)
-    master_file_handler.setFormatter(LOG_FORMAT)
+    master_file_handler.setFormatter(log_format)
 
     # create console logger
     console_handler = logging.StreamHandler()
     console_handler.setLevel(level)
-    console_handler.setFormatter(LOG_FORMAT)
+    console_handler.setFormatter(log_format)
 
     # return loggers with both logging handlers
     logger.addHandler(console_handler)
