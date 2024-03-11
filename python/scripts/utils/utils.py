6a4875b4f15f0e91816a960c71d3d4109b817bde
Daniel Santander <dan.santander@digitaldefense.com>
2024-03-11 10:06:57 -0500

    add method to sort dictionary objects

--- python/scripts/utils/utils.py
+++ python/scripts/utils/utils.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 #!/usr/bin/python3
 
+from typing import Literal
+
 def diff_lists(list_a:list, list_b:list, verbose:bool=False):
     """
     Return list of items in list_a which are not in list_b.
@@ -13,6 +15,16 @@ def diff_lists(list_a:list, list_b:list, verbose:bool=False):
         print(f"set two length: {len(s2)}")
         print(f"differences of sets length: {len(s2)}")
     return results
+
+def sort_dict(data:dict, sort_by:Literal['key', 'value']='key')->dict:
+    """
+    src: https://www.geeksforgeeks.org/python-sort-python-dictionaries-by-key-or-value/
+    """
+    # TODO: implement ability to sort by either key OR values.
+    sorted_key_list = list(data.keys())
+    sorted_key_list.sort()
+    sorted_dict = { i: data[i] for i in sorted_key_list }
+    return sorted_dict
 
 if __name__ == '__main__':
     import argparse
@@ -29,7 +41,14 @@ if __name__ == '__main__':
     args = parser.parse_args()
     in_verbose = bool(args.verbose is True)
 
-    list_a = ['a','b','c']
-    list_b = ['a','b','d']
-    results = diff_lists(list_a, list_b, verbose=in_verbose)
-    print(results)
+    results = None
+    if args.input == 'diff_lists':
+        list_a = ['a','b','c']
+        list_b = ['a','b','d']
+        results = diff_lists(list_a, list_b, verbose=in_verbose)    # contents in list_a which are not in list_b
+    else:
+        data = { "3": "three", "1": "one", "2": "two" }
+        results = sort_dict(data)
+
+    print(f"data: {data}\n")
+    print(f"results: \n{results}\n")
