b86a797e973b154a610c7f79018d973577f3c905
danielsantander <santander9108@gmail.com>
2025-04-19 21:41:45 -0500

    add script to run python api test cases, curently running at 85% coverage

---
+++ python/api/tests/run_tests.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+python3 -m coverage run --source="../" -m unittest discover .
+pid=$!
+
+
+# kill -0 -> doesn't send a signal, but returns exit status 0 if process exists else non-zero
+while kill -0 $pid >/dev/null 2>&1; do
+  echo "Process $pid is still running..."
+  sleep 5
+done
+
+# output results
+if [ ! -d "coverage_files" ]; then
+  echo "coverage_files/ does not exist, creating directory ... "
+  mkdir coverage_files
+fi
+resport_results=$(coverage report)
+echo "$resport_results" > "coverage_files/$(date +%Y%m%d%H%M%S)--report.txt"
+coverage annotate -d "coverage_files/"
+echo ""
+echo ""
+echo "REPORT RESULTS"
+echo "----------------"
+echo ""
+echo "${resport_results}"
